f5c298a8f1e527a7a1b976cd5f9695d8c98548a2
DragonHunter71 <68894120+DragonHunter71@users.noreply.github.com>
2023-06-29 12:49:14 +0530

    spark_vayu.mk to superior_vayu.mk

--- superior_vayu.mk
+++ superior_vayu.mk
@@ -11,14 +11,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common ColtOS configurations
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+# Inherit common SuperiorOS configurations
+$(call inherit-product, vendor/superior/config/common.mk)
 
-PRODUCT_NAME := spark_vayu
+PRODUCT_NAME := superior_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Superior Flags
+BUILD_WITH_GAPPS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+MAINTAINER := DragonHunter71
+TARGET_ENABLE_BLUR := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
